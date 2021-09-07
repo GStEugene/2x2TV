d1a1d9bbb09c64283f80c2e23d173a814ed946bd
GStEugene <djakdragon51@gmail.com>
2021-09-07 20:54:55 +0300

    Fix

--- 2x2TV_Script.py
+++ 2x2TV_Script.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 import sys
+import os
 
 print(int(time.time()) + 3000)
 href = "https://bl.zxz.su/live/317805/HLS/4614144_3,2883584_2,1153024_1/" + str(int(time.time()) + 3000) + "/9ffdf74b4268d28648f06f10aa5e4aa5/playlist.m3u8"
@@ -12,6 +13,9 @@ with open('2x2.ts', 'w') as ouf:
 #EXTVLCOPT:network-caching=1000
 ''' + href)
 
-opener = "open" if sys.platform == "darwin" else "xdg-open"
-
-subprocess.call([opener, '2x2.ts'])
+if sys.platform == 'linux':
+    subprocess.call(["xdg-open", '2x2.ts'])
+elif sys.platform == 'win32':
+    os.startfile('2x2.ts')
+else:
+    subprocess.call(["open", '2x2.ts'])
